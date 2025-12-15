66eb9ad35edba89552046c1958649c7fd02f7b6d
LiYunLong <2516007911@qq.com>
2025-12-16 00:16:15 +0800

    Initial commit

---
+++ python_basic/类/创建实例.py
@@ -0,0 +1,8 @@
+class Person:
+    def __init__(self,name,age,gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+p1 = Person("张三",18,"男")
+p2 = Person("王五",19,"女")
